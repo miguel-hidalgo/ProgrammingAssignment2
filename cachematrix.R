3083cbaf5bf29b031f77078006759f5d7733c11a
Miguel Hidalgo <miguel.hidalgo@aggreko.com>
2015-09-13 11:39:02 -0500

    cacheTester  Function Created
    Created a test unit function named cacheTester.  This functions call
    makeCacheMatrix once, then call solveCache twice and do logical
    comparisons of the inverted elements while printing inverted matrices
    and logical vectors to verify visually the results.

--- cachematrix.R
+++ cachematrix.R
@@ -53,13 +53,15 @@ cacheSolve <- function(x, ...) {
  
       ## Call the getmatrix function to get the inversed of the original matrix
       mtx <- x$getmatrix()
-      
+  
       ## If the matrix returned equals NULL, then it has not been cached.
       ## If the matrix returned is not equal to NULL, returns the cached inversed matrix
       if(!is.null(mtx)) {
-        message("getting cached data")
-        ## returning the inversed matrix from cache
-        return(mtx)
+          message("getting cached data")
+          ## returning the inversed matrix from cache
+          return(mtx)
+      } else {
+        message("data not in cache")
       }
       ## if this part of the code is executed, it means that
       ## the Matrix has not been inversed.  So, get original matrix
@@ -75,3 +77,57 @@ cacheSolve <- function(x, ...) {
       ## return inversed Matrix to be used by the caller of cacheSolve()
       return (mtx)
 }
+
+##
+## Function Name: cacheTester 
+## Purpose......: This function, test the cache functions 
+## .............: Creates a squared Matrix from the code in the R help for solve()
+## .............: Solves (Inverses the created Matrix) for comparison
+## .............: Calls makeCacheMatrix with the square matrix
+## .............: calls cacheMatrix with the square matrix message not in cache appears
+## .............: calls cacheMatrix a second and third time with the square matrix message getting cache data appears
+## Return ......: Output the inversed matrix to screen before cach and after cache with the 
+## .............: Messages Data Not in Cache and getting cached data.
+##
+cacheTester <- function() {
+ 
+  print("Begin Test")
+  ## Create square Matrix
+  hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }   
+  h8 <- hilbert(8); h8
+  ## create and inversed matrix to compare later
+  sh8 <-solve(h8)
+  ## Create cacheMatrix object
+  x<-makeCacheMatrix(h8)
+  ## Get the original data from the matrix object
+  odata<-x$get()
+  ## create a logig array that records all the compared values in the matrices
+  lod<-odata==h8
+  ## checks that the matrix in the cache is identical to the original passed
+  if (all(lod)) print ("TRUE") else print("FALSE")
+  
+  ## Call cacheSolve to Invert Matrix but check if it has been cached
+  im<-cacheSolve(x)
+  ## print inverted matrix
+  print(im)
+  ## Call cacheSolve to Invert Matrix but check if it has been cached
+  imc<-cacheSolve(x)
+  ## print inverted matrix for visualcomparison.
+  print(imc)
+  ## Compare inversed matrices and return the logical matrix with all elements set to
+  ## TRUE if they are equal
+  if (all(im==imc)) print ("TRUE") else print("FALSE")
+  print(im==imc)
+  ## Call cacheSolve to Invert Matrix but check if it has been cached
+  imc2<-cacheSolve(x)
+  ## print inverted matrix for visualcomparison.
+  print(imc2)
+  ## Compare inversed matrices and return the logical matrix with all elements set to
+  ## TRUE if they are equal
+  if (all(im==imc2)) print ("TRUE") else print("FALSE")
+  print(im==imc2)
+  print("Test Completed")
+  
+}
+
+
